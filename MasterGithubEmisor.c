954e7d5cf4626c5fa757168236da1c63a4623050
JoseDamianCardenas <37005472+JoseDamianCardenas@users.noreply.github.com>
2019-02-02 14:21:26 -0600

    Add files via upload

---
+++ MasterGithubEmisor.c
@@ -0,0 +1,21 @@
+#include <16F876.h>
+#device adc=10
+#fuses  XT
+#use    delay( clock = 4000000 )
+
+int i,adc=7;
+void main(void){
+
+//SET_TRIS_A (0xff);      /* 0xff --> TRISA */
+SET_TRIS_B (0x00);      /* 0x00 --> TRISB */
+SET_TRIS_C (0x00);      /* 0x00 --> TRISB */
+output_low(PIN_C7); 
+setup_adc_ports(ALL_ANALOG);  //Se ,configuran todas las entradas del puerto a como analógicas
+setup_adc(ADC_CLOCK_INTERNAL);  //Se activa el reloj interno del PIC
+   
+    for(i = 1;i<=adc;i++)
+      {
+      output_toggle(PIN_C7);
+      delay_us(150);
+      }
+}
